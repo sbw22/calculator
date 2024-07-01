0972e2301025e6fae2e14c61978f410618ab5448
Spencer Weishaar <sbweishaar@icloud.com>
2024-06-30 23:02:21 -0500

    finalized all logic for calculator calculations. finished style to mimic iphone calc style

--- calc.js
+++ calc.js
@@ -2,34 +2,13 @@ let num1 = undefined;
 let num2 = undefined;
 let op;
 let opClicked = false;
-/*
-let adding = () => {
-    console.log("adding called. num1 = " + num1 + ", num2 = " + num2);
-    console.log('in add');
-    num1 += num2;
-    num2 = undefined;
-    return num1;
-}
-let subtracting = () => {
-    console.log("subtracting called. num1 = " + num1 + ", num2 = " + num2);
-    num1 -= num2;
-    num2 = undefined;
-    return num1;
-}
-let multiplying = () => {
-    console.log("multiplying called. num1 = " + num1 + ", num2 = " + num2);
-    num1 *= num2;
-    num2 = undefined;
-    return num1;
-}
-let dividing = () => {
-    console.log("dividing called. num1 = " + num1 + ", num2 = " + num2);
-    num1 /= num2;
-    num2 = undefined;
-    return num1;
-} */
+
+let op1 = undefined;
+let op2 = undefined;
 
 let display = document.querySelector('#display');
+
+let opDiv = document.querySelector('#opDiv');
 
 let ac = document.querySelector("#ac");
 let sign = document.querySelector("#sign");
@@ -54,37 +33,46 @@ let numsA = [zero, one, two, three, four, five, six, seven, eight, nine];
 let opsA = [add, subtract, divide, multiply, equal];
 
 let addNum = {name: "add", symbol: "+", call() {
-    console.log("adding called. num1 = " + num1 + ", num2 = " + num2);
+    console.log("addNum called. num1 = " + num1 + ", num2 = " + num2);
     console.log('in add');
-    num1 += num2;
+    num1 = num1 + num2;
     num2 = undefined;
     return num1;
 }};
-let addMinus = {name: "subtract", symbol: "-", call() {
-    console.log("subtracting called. num1 = " + num1 + ", num2 = " + num2);
-    num1 -= num2;
+let minusNum = {name: "subtract", symbol: "-", call() {
+    console.log("subtractNum called. num1 = " + num1 + ", num2 = " + num2);
+    num1 = num1 - num2;
     num2 = undefined;
     return num1;
 }};
-let addMultiply = {name: "multiply", symbol: "*", call() {
-    console.log("multiplying called. num1 = " + num1 + ", num2 = " + num2);
-    num1 *= num2;
+let multiplyNum = {name: "multiply", symbol: "*", call() {
+    console.log("multiplyNum called. num1 = " + num1 + ", num2 = " + num2);
+    num1 = num1 * num2;
     num2 = undefined;
     return num1;
 }};
-let addDivide = {name: "divide", symbol: "/", call() {
-    console.log("dividing called. num1 = " + num1 + ", num2 = " + num2);
-    num1 /= num2;
+let divideNum = {name: "divide", symbol: "/", call() {
+    console.log("divideNum called. num1 = " + num1 + ", num2 = " + num2);
+    num1 = num1 / num2;
     num2 = undefined;
     return num1;
-} };
+}};
+
+let equalNum = {name: "equal", symbol: "=", call() {
+    console.log("equalNum called. num1 = " + num1 + ", num2 = " + num2);
+    return 404;
+}};
+
+// when equal is clicked, set num1 and num2
+//if number is clicked after equal is clicked, set num1 equal to the number and num2 as undefined, and set op1 and op2 to undefined
+//if op is clicked after equal is clicked, continue like other ops
 
 let opsO = [
     addNum,
-    addMinus,
-    addMultiply,
-    addDivide,
-   // {name: "equal", symbol: "=", link: equal}
+    minusNum,
+    multiplyNum,
+    divideNum,
+    equalNum
 ];
 
 let number = document.querySelectorAll('.num');
@@ -93,60 +81,86 @@ let ops = document.querySelectorAll('.op');
 
 let opp;
 
-// when first numbers are clicked, store values in a.
-// when operator is clicked, finish storing values in a and store operator value in mid.
-// when second numbers are clicked, store values in b.
-// when second operator is clicked, finish storing values in b an d store second operator in end.
+
+//sets initial value of display to 0
+display.innerHTML = '0';
+
+
+
 
 for(let i = 0; i<10; i++) { //adds event listener to each individual number button
     numsA[i].addEventListener("click", () => {
         if( opClicked === true){
-            display.innerHTML = "";
+            display.innerHTML = "0"; // COME BACK TO THIS
+            if (op1 == "="){
+               op2 = undefined;
+               // num2 = undefined;
+            }
         }
-        //console.log(i + " clicked");
-        display.innerHTML += i;
-        //operate(num);
+        if (display.innerHTML == '0'){
+            display.innerHTML = "";
+        } 
+
+        display.innerHTML += i; //add num to display
         opClicked = false;
     });
 }
 
 ac.addEventListener("click", () => { //ac event listener that deletes display characters when pressed
     console.log('ac clicked');
-    display.innerHTML = "";
+    display.innerHTML = "0";
+    num1 = undefined;
+    num2 = undefined; 
+    op1 = undefined;
+    op2 = undefined; 
+    //ADD OP1 and OP2 ????
 });
+
+/////////////////////////////////////////////////////////////////////////
+
+// 1)
+//  A) When equal is pressed, 
 
 ops.forEach((opp) => { //event listener for operator click. Also assigns values to num1 and num2 on click
     opp.addEventListener("click", () => {
         // console.log(opp.innerHTML + " clicked");
         opClicked = true;
 
-        op = opp.innerHTML;
-        if(num1 == undefined) {
+        if(num1 == undefined || op1 == '=') { //setting num1 and num2
             num1 = display.innerHTML;
-            parseInt(num1);
+            num1 = parseInt(num1);
         } else {
             num2 = display.innerHTML;
-            parseInt(num2);
+            num2 = parseInt(num2);
+        }
+
+        if(op1 === undefined || op1 == '='){
+            op1 = opp.innerHTML;
+        } else {
+            op2 = opp.innerHTML;
         }
 
         opsO.forEach((opp) => {
-            if (opp.symbol == op){
-                console.log("opp symbol = " + opp.symbol);
-                if(num2 != undefined){
-                    console.log("Call: " + opp.call());
+            if (opp.symbol == op1){
+                if(num2 != undefined && op2 != undefined){
+                    let call = opp.call();
+                    console.log("Call: " + call);
+                    display.innerHTML = call;
+
+                    op1 = op2;
+                    op2 = undefined;
                 }
             }
-        })
+        });
         
     });
 });
 
-document.addEventListener("click", () => { //event listener for click on document
-    console.log("op = " + op);
-    // console.log("op data type = " + typeof(op));
-   // console.log("document clicked. num1 = " + num1 + ", num2 = " + num2);
-});
+//console.log('cold call to ops[0] = ' + addNum.call());
 
+if(opClicked == true){
+    opDiv.style.backgroundColor = "green";
+} 
 
 
 
